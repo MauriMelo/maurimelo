83fc29ceaddf637c044ae6b1fe08d0b638bb3f65
Mauri Martins de Melo <mauri@coopercarga.com.br>
2025-01-05 19:18:24 -0300

    my curriculum page

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import path from 'path';
+import handlebars from 'vite-plugin-handlebars';
+
+export default {
+  root: path.resolve(__dirname, 'src'),
+  plugins: [
+    handlebars({
+      partialDirectory: path.resolve(__dirname, 'src/partials'),
+    })
+  ],
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      '~fontawesome': path.resolve(__dirname, 'node_modules/fontawesome'),
+    }
+  },
+  server: {
+    port: 8080,
+    hot: true
+  },
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/index.html'),
+        // 'en-us': path.resolve(__dirname, 'src/en-us.html'),
+      },
+    },
+  },
+}
